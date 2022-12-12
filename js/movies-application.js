0b7996cf32aab189e70fe7dfcad17796efd4ca9e
Jonathan Vargas <jonatan.vargas3339@gmail.com>
2022-12-12 11:05:06 -0600

    added titles and ratings

--- js/movies-application.js
+++ js/movies-application.js
@@ -3,30 +3,56 @@
 fetch("https://cloud-happy-fox.glitch.me/movies").then(resp => resp.json()).then(data => console.log(data))
 
 var toggle = $('#reload').toggleClass('loadingimage')
+var addRating = document.getElementById("added-movie")
+var addTitle = document.getElementById('submit-title');
+var submitButton = document.querySelector('#submit-movie');
 
 
-
-$('#reload').click(function(){
-    getList()
+$('#reload').click(function () {
+	getList()
 })
 
-function getList(){
-    $('#reload').toggleClass('loadingimage')
-    var movielist = document.getElementById('movielist');
+function getList() {
+	$('#reload').toggleClass('loadingimage')
+	var movielist = document.getElementById('movielist');
 // .then(data => lastcommit.innerText = ('Date of last commit: ' + data[0].commit.author.date))
-    fetch("https://cloud-happy-fox.glitch.me/movies").then(resp => resp.json()).then(data => movielist.innerText = data[0].title)
-        .then(() => $('#reload').toggleClass('loadingimage')
-            // .then(data => console.log(data))
-
-)
-
-
-    // if ( display === true ) {
-    //     $( "#loadingimage" ).show();
-    // } else if ( display === false ) {
-    //     $( "#loadingimage" ).hide();
-    // }
+	fetch("https://cloud-happy-fox.glitch.me/movies").then(resp => resp.json())
+		.then(data => {
+			let html = '';
+			for (i = 0; i < data.length; i++) {
+				html += `<h1>${data[i].title} </h1>`
+				html += `<h3>${data[i].rating} </h3>`
+			}
+			movielist.innerHTML = html;
+		})
+		.then(() => $('#reload').toggleClass('loadingimage')
+		)
 }
+// ADD Movie
+
+
+function addMovie(m) {
+	m.preventDefault();
+	let movieObj = {
+	// 	id: coffees.length + 1,
+		title: addTitle.value,
+		rating: addRating.value
+	};
+	// Fetch post to movies json
+	data.push(movieObj);
+	// Display add coffees
+	// dbody.innerHTML = renderCoffees(coffees);
+	// localStorage.setItem(coffeeObj.id.toString(), JSON.stringify(coffeeObj)); //key(id): "coffeeObj"
+	JSON.parse(window.localStorage.getItem(coffeeObj.id.toString()));
+	addTitle.value = ''; //resets typed value
+}
+submitButton.addEventListener('click', addMovie);
+
+// Todo
+//  Refresh button for API when list edited
+//  User edit movies attributes in real time
+//  Allow user to remove movie from list
+
 // getList();
 
 // HOW TO GET
